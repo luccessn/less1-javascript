eafe0fffbdacffcf4a5d0a43a4fdada4c61c3a6c
luccessn <gulualuka0@gmail.com>
2024-09-17 11:46:52 -0700

    If/else/switch statemants

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1
+let x = 100;
+if (x < 50) {
+  console.log(`x less than 50 `);
+} else if (x > 20) {
+  console.log(`x much more than 20 `);
+} else {
+  console.log(`error`);
+}
+// 2
+let z = `mariami`;
+if (z == `mariami`) {
+  console.log(`TRUE`);
+} else {
+  console.log(`FALSE`);
+}
+// 3
+let userName = `mariami`;
+switch (userName) {
+  case `mariami`:
+    console.log(`True`);
+    break;
+  default:
+    console.log(`False`);
+}
